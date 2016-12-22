c0917eb2329c6eec3d132f54e87beb30a27f31ec
nokiddn <rmalukas@gmail.com>
2016-12-22 14:54:13 +0200

    Add kindergarten info and finish user input

--- ui.R
+++ ui.R
@@ -9,7 +9,8 @@
 
 library(shiny)
 
-seniunija <- readRDS("dists.RDS")
+seniunija <- readRDS("dists.RDS") %>% sort
+darzeliai <- readRDS("darzeliai.RDS") %>% sort
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -20,27 +21,45 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      # strong("Gimimo data"),
-      # selectizeInput("birthyear", label = "Metai", 
-      #                choices = 2009:2017),
-      # selectizeInput("birthmonth", label = "Mėnuo", 
-      #                choices = 1:12),
-      # selectizeInput("birthday", label = "Diena",
-      #                choices = 1:30),
       dateInput("birthddate", "Gimimo data", min = "2009-01-01",
                 max = "2030-01-01"),
       selectizeInput("district", label = "Seniūnija",
                      choices = seniunija),
-      checkboxInput("city", label = "Deklaruotas mieste"),
+      selectizeInput(
+        "city", label = "Deklaruoti mieste", 
+        choices = setNames(1:3, c("Abu tėvai", "Vienas iš tėvų",
+                                  "Nei vienas iš tėvų"))),
+      checkboxInput(
+        "2years", 
+        label = "Bent vienas iš tėvų registruotas Vilniuje ne mažiau 2 metų?"),
+      checkboxInput(
+        "school",
+        label = "Vienas iš tėvų mokosi bendrojo ugdymo mokykloje?"),
+      # checkboxInput("city", label = "Deklaruotas mieste"),
       checkboxInput("3more", label = "3 ir daugiau"),
       checkboxInput("unable", label = "Žemas darbingumas"),
-      checkboxInput("lonely", label = "Augina 1 iš tėvų"),
+      conditionalPanel(
+        condition = "input.city == 1", 
+        checkboxInput("lonely", label = "Augina 1 iš tėvų")
+      ),
+      checkboxInput("otherkids", label = "Ar turite kitų vaikų Vilniaus darželiuose?"),
+      conditionalPanel(
+        condition = "input.otherkids",
+        selectizeInput("otherkg", label = "Kuriuose?",
+                       choices = sort(darzeliai), multiple = TRUE)
+      ),
+      selectizeInput(
+        "language", label = "Kalba", 
+        choices = setNames(1:4, c("Hebrajų", "Lenkų", "Lietuvių", "Rusų")),
+        selected = 3),
+      checkboxInput("special", label = "Specialūs poreikiai"),
       actionButton("go", label = "Prognozuoti")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
        #plotOutput("distPlot")
+      tableOutput("table")
     )
   )
 ))
