fd3a5299a4babc55e661f2e766041a5dbd8e93b1
philjonas <philjonascode@gmail.com>
2017-01-24 10:18:36 +0200

    google analytics key added

--- ui.R
+++ ui.R
@@ -17,6 +17,14 @@ shinyUI(fluidPage(
   
   tags$link(rel = "stylesheet", type = "text/css", href = "maps.css"),
   tags$script(src = "addMap.js"),
+  tags$script("
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72198439-2', 'auto');
+  ga('send', 'pageview');"),
   
   # Application title
   tags$img(src='vilnius-logo.svg', class="city-logo"),
